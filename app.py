9e4e6e0f37bf3a5a6d042f8e79f59bb962e61bd0
Gian Marco Arteaga Álvarez <gian.arteaga@utec.edu.pe>
2023-08-24 19:41:30 -0500

    0.0.2

--- app.py
+++ app.py
@@ -4,21 +4,26 @@ import uuid
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def index():
-    return '¡Hola, mundo! Esta es mi primera API con Flask.'
+    return '¡Hola, mundo! Esta es mi primera API con Flask y veremos animes.'
 
+id = uuid.uuid1()
+anime = {"id":id, "titulo":"Dragon Ball",
+        "poster":"https://s4.anilist.co/file/anilistcdn/user/avatar/large/default.png",
+        "genero":"Action","rating":10000,"reviews":10,"season":"GIT","tipo":"serie"}    
 
 @app.route('/anime', methods =['GET'])
-def crear_anime():
-    u = uuid.uuid1()
-    anime = {"id":u, "titulo":"Dragon Ball",
-             "poster":"https://s4.anilist.co/file/anilistcdn/user/avatar/large/default.png",
-             "genero":"Action","rating":10000,"reviews":10,"season":"GIT","tipo":"serie"}
-    
-    print(list(anime.values()))
-    
+def list_animes():
     return list(anime.values())
+
+@app.route('/anime/<id>', methods = ['POST'])
+def create_anime():
+    pass
+
+@app.route('/anime/<id>', methods = ['DELETE'])
+def delete_anime():
+    anime.clear()
 
 if __name__ == '__main__':
     app.run(debug=True)
