e26f91c195306afeed07becee0ce5bd6f0a26435
Gian Marco Arteaga Álvarez <gian.arteaga@utec.edu.pe>
2023-08-28 20:55:37 -0500

    Versión 0.0.7

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask
 import uuid
 
 app = Flask(__name__)
+# Modelo
+anime = [{"id":id, "titulo":"Dragon Ball",
+        "poster":"https://s4.anilist.co/file/anilistcdn/user/avatar/large/default.png",
+        "genero":"Action","rating":10000,"reviews":10,"season":"GIT","tipo":"serie"}]
 
 # Controlador (API)
 @app.route('/', methods = ['GET'])
@@ -11,11 +15,6 @@ def index():
     return '¡Hola, mundo! Esta es mi primera API con Flask y veremos animes.'
 
 id = uuid.uuid1()
-
-anime = [{"id":id, "titulo":"Dragon Ball",
-        "poster":"https://s4.anilist.co/file/anilistcdn/user/avatar/large/default.png",
-        "genero":"Action","rating":10000,"reviews":10,"season":"GIT","tipo":"serie"}]
-
 
 @app.route('/anime', methods = ['GET'])
 def list_animes():
@@ -77,7 +76,6 @@ def delete_anime(id):
     return "No se encontró el anime!!"
             
     
-
 if __name__ == '__main__':
     app.run(debug=True)
 
